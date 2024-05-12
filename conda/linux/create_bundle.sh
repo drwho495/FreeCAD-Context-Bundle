5cb0bfccfc5b3ca945674e87143e7eeec850b749
drwho495 <70452450+drwho495@users.noreply.github.com>
2024-05-12 13:08:07 -0500

    Update create_bundle.sh
    More experimentation #1

--- conda/linux/create_bundle.sh
+++ conda/linux/create_bundle.sh
@@ -8,7 +8,7 @@ fi
 conda_env="AppDir/usr"
 echo -e "\nCreate the environment"
 
-packages="ondsel-es=*.pre occt vtk python=3.11 blas=*=openblas numpy \
+packages="occt vtk python=3.11 blas=*=openblas numpy \
           matplotlib-base scipy sympy pandas six pyyaml pycollada lxml \
           xlutils olefile requests blinker opencv nine docutils \
           opencamlib calculix ifcopenshell lark appimage-updater-bridge"
